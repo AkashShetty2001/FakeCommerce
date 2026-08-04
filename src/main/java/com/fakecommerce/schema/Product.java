f8d05eae9852a73c3e9252ae0a9bf27b6a45cce8
AkashShetty2001 <akashkd9911@gmail.com>
2026-08-04 17:08:21 +0530

    added an @DynamicUpdate on product schema for minimal updates to db

--- src/main/java/com/fakecommerce/schema/Product.java
+++ src/main/java/com/fakecommerce/schema/Product.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 
 import java.math.BigDecimal;
 
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@DynamicUpdate
 @Entity
 @Table(name = "products")
 public class Product {
